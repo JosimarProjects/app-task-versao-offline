8ddec26601329825b13c06de7f94adbc8e498597
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-09-02 19:41:51 -0300

    add icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,29 +1,73 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default props => {
     return (
         <View style={styles.container}>
-            <Text style={styles.desc}>{props.desc}</Text>
-            <Text style={styles.date}>{props.estimateAt.toString()}</Text>
-            <Text style={styles.date}>{props.doneAt ? props.doneAt.toString() : 'Não concluído'}</Text>
+            <View style={styles.checkContainer}>
+                {getCheckView(props.doneAt)}
+            </View>
+            <View>
+                <Icon name="rocket" size={30} color="#900" />
+
+                <Text style={styles.desc}>{props.desc}</Text>
+                <Text style={styles.date}>{props.estimateAt.toString()}</Text>
+            </View>
         </View>
     );
 }
 
+function getCheckView(doneAt) {
+    if (doneAt != null) {
+        return (
+            <View style={styles.done}>
+                <Icon name='check' size={20} color='#FFF' />
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.pending}></View>
+        );
+    }
+}
+
 const styles = StyleSheet.create({
     container: {
-        margin: 10,
-        padding: 10,
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    checkContainer: {
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
     },
     desc: {
         fontSize: 18,
         fontWeight: 'bold',
-        color: '#000000',  // Cor preta
+        color: '#000000',
     },
     date: {
         fontSize: 14,
         fontWeight: 'bold',
-        color: '#000000',  // Cor preta
+        color: '#000000',
     },
+    pending: {
+        width: 25,
+        height: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555',
+    },
+    done: {
+        width: 25,
+        height: 25,
+        borderRadius: 13,
+        backgroundColor: '#4d7031',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
