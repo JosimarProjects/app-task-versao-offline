23117792df3d490b61940114a626638d850092de
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-10-05 09:51:56 -0300

    adicionando task e listando pelo backend

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Pl
 import commonStyles from "../commonStyles";
 import todayImage from '../../assets/imgs/today.jpg';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Axios from "axios";
+import { server, showError } from "../common";
 import 'moment/locale/pt-br';
 import moment from 'moment';
 import Task from "../components/Task";
@@ -22,10 +24,29 @@ export default class TaskList extends Component {
     }
 
     componentDidMount = async () => {
-        const stateString = await AsyncStorage.getItem('tasksState');
-        const state = JSON.parse(stateString) || initialState;
-        this.setState(state, this.filterTasks);
+        const stateString = await AsyncStorage.getItem('tasksState')
+        const savedState = JSON.parse(stateString) || initialState
+        this.setState({
+            showDoneTasks: savedState.showDoneTasks
+        }, this.filterTasks)
+
+        this.loadTasks()
     }
+
+    loadTasks = async () => {
+        try {
+            const maxDate = moment().format('YYYY-MM-DD 23:59:59')
+            ;
+            const res = await Axios.get(`${server}/tasks?date=${maxDate}`)
+            this.setState({ tasks: res.data }, this.filterTasks)
+            
+        }catch(error) {
+            showError(error);
+        }
+        
+    }
+
+    
 
     componentDidUpdate = async () => {
         await AsyncStorage.setItem('tasksState', JSON.stringify(this.state));
@@ -45,7 +66,9 @@ export default class TaskList extends Component {
         }
 
         this.setState({ visibleTasks });
-        AsyncStorage.setItem('tasksState', JSON.stringify(this.state.tasks));
+        AsyncStorage.setItem('tasksState', JSON.stringify({
+            showDoneTasks:this.state.showDoneTasks
+        }));
     }
 
     toggleTask = taskId => {
@@ -58,21 +81,32 @@ export default class TaskList extends Component {
         this.setState({ tasks }, this.filterTasks);
     }
 
-    addTask = newTask => {
+    addTask = async newTask => {
         if (!newTask.desc || !newTask.desc.trim()) {
             Alert.alert('Dados Inválidos', 'Descrição não informada!');
             return;
         }
 
-        const tasks = [...this.state.tasks];
+       /* const tasks = [...this.state.tasks];
         tasks.push({
             id: Math.random(),
             desc: newTask.desc,
             estimateAt: newTask.date,
             doneAt: null
-        });
+        });*/
 
-        this.setState({ tasks, showAddTask: false }, this.filterTasks);
+        try{
+            await Axios.post(`${server}/tasks`, {
+                desc: newTask.desc,
+                estimateAt: newTask.date
+            })
+            
+            this.setState({ showAddTask: false }, this.loadTasks);
+
+        }catch(error) {
+            showError(error);
+        }
+        
     }
 
     deleteTask = taskId => {
