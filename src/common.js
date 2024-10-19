53602322597cf839cac4f4746f2d56372939d773
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-10-19 10:29:22 -0300

    finish projetct

--- src/common.js
+++ src/common.js
@@ -3,7 +3,11 @@ import { Alert, Platform } from "react-native";
 const server = Platform.OS === 'ios' ? 'http://244.178.44.111:3000':'http://192.168.15.60:3000'
 
 function showError(msg) {
-    Alert.alert(`Ops! Ocorreu um erro: ${msg}`);
+    if(msg.response && msg.response.data) {
+        Alert.alert(`Ops! Ocorreu um erro: ${msg.response.data}`)
+    }else {
+        Alert.alert(`Ops! Ocorreu um erro: ${msg}`);
+    }
 }   
 
 function showSuccess(msg) {
